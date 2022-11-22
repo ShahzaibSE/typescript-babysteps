21d4e4c62dd34e95a73d2b5e79f82872931e5ce4
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-11-22 21:20:07 +0500

    [MOD] Added another file in module system

--- step00-ECMA-moduling/index.js
+++ step00-ECMA-moduling/index.js
@@ -4,4 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const first_1 = __importDefault(require("./first"));
+const second_1 = require("./second");
 console.log(first_1.default);
+console.log(second_1.b);
+console.log(second_1.c);
---
+++ step00-ECMA-moduling/second.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.c = exports.b = void 0;
+exports.b = 0b1111; // binary number.
+exports.c = 100;
