5ce02e92f59ebe48f91135ac69da042cef5ce49a
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-11-22 21:17:45 +0500

    [MOD] Added another file in module system

--- step00-ECMA-moduling/index.ts
+++ step00-ECMA-moduling/index.ts
@@ -1,3 +1,6 @@
 import a from "./first";
+import {b,c} from "./second";
 
 console.log(a);
+console.log(b);
+console.log(c);
---
+++ step00-ECMA-moduling/second.ts
@@ -0,0 +1,2 @@
+export let b = 0b1111; // binary number.
+export let c = 100;
